7c0ad4a163590ed1c412c597b51ce7f80f366eb5
Bos Eriko Reyes <bos@eriko.ph>
2026-09-10 20:26:54 +0800

    📝 rename flight component reference

--- app/flappy/page.tsx
+++ app/flappy/page.tsx
@@ -1,5 +1,5 @@
-import TigerFlight from "./tiger-flight";
+import TwisWuaFlight from "./tiger-flight";
 
 export default function FlappyPage() {
-  return <TigerFlight />;
+  return <TwisWuaFlight />;
 }
